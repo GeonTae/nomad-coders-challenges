912a1450994223e655582cded1d17229ca9c1990
Tae <rjsxo51@gmail.com>
2024-10-19 20:38:48 -0700

    update

--- js-8-xmas_dday/main.js
+++ js-8-xmas_dday/main.js
@@ -1,7 +1,4 @@
-const clock = document.querySelector('.js-clock');
-
-
-
+const clock = document.querySelector(".js-clock");
 
 function getClock() {
   // const date = new Date();
@@ -16,24 +13,26 @@ function getClock() {
 
   let xmasDate;
 
-  if (monthNow === 11 & dateNow > 24) {
-    xmasDate = new Date(timeNow.getFullYear() + 1, 11, 24,); //month,date,hour,min,sec
-  }
-  else {
-    xmasDate = new Date(timeNow.getFullYear(), 11, 24,); //month,date,hour,min,sec
+  if ((monthNow === 11) & (dateNow > 24)) {
+    xmasDate = new Date(timeNow.getFullYear() + 1, 11, 24); //month,date,hour,min,sec
+  } else {
+    xmasDate = new Date(timeNow.getFullYear(), 11, 24); //month,date,hour,min,sec
   }
 
   const difference = xmasDate - timeNow;
 
-  const days = Math.floor(difference/(24*60*60*1000));
-  const hours = Math.floor((difference%(24*60*60*1000))/(60*60*1000)).toString().padStart(2,"0");
-  const minutes = Math.floor((difference%(60*60*1000))/(60*1000)).toString().padStart(2,"0");
-  const seconds = Math.floor((difference%(60*1000))/(1000)).toString().padStart(2,"0");
-
-  
-  
-
-
+  const days = Math.floor(difference / (24 * 60 * 60 * 1000));
+  const hours = Math.floor(
+    (difference % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
+  )
+    .toString()
+    .padStart(2, "0");
+  const minutes = Math.floor((difference % (60 * 60 * 1000)) / (60 * 1000))
+    .toString()
+    .padStart(2, "0");
+  const seconds = Math.floor((difference % (60 * 1000)) / 1000)
+    .toString()
+    .padStart(2, "0");
 
   clock.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
   // clock.innerText =`${hours}:${minutes}:${seconds}`;
